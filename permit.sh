10c7fd963862339d627bb6a1d5b076b051e31815
Arjun G <arjungirirajkumar@gmail.com>
2023-05-16 14:29:24 +0000

    Done permit using chmod and chown only

--- permit.sh
+++ permit.sh
@@ -3,40 +3,68 @@
 filePath="/home/arjun/Documents/delta-sysad/studentDetails.txt"
 # read -p 'Enter file path: ' filePath
 
-# Set HAD as owner and root as group owner of HAD and Hostel home directories and subdirectories
-# Disable permissions for anyone else
-chown -R HAD:root /home/HAD
-chmod 770 /home/HAD
+# Groups: 
+# HAD + Each Hostel
+# All students
+
+#Creating each group
+groupadd allStudents
+while read line
+do
+
+    read -a studentData <<< "$line"
+    name=${studentData[0]}
+    hostel=${studentData[2]}
+
+    #HADHostel groups
+    if !(grep -q "HAD$hostel" /etc/group) ;
+    then
+        groupadd "HAD$hostel"
+    fi
+done < $filePath
+
+#Change owners
+chown -R HAD:allStudents /home/HAD
+
 while read line
 do
     read -a studentData <<< "$line"
     hostel=${studentData[2]}
-    chown -R HAD:root /home/$hostel 
-    chmod -R 770 /home/$hostel
+    chown -R root:HAD$hostel /home/$hostel
 done < $filePath
 
-# Using ACL (Access Control List) setting up all permissions
 while read line
 do
     read -a studentData <<< "$line"
     name=${studentData[0]}
-    rollno=${studentData[1]}
     hostel=${studentData[2]}
     roomno=${studentData[3]}
-
-    # Students home directory permission
-    setfacl -R -m u:$name:r-- /home/$hostel/$roomno/$name
-
-    # announcements.txt and feeDefaulters.txt permission
-    setfacl -m u:$name:r-- /home/$hostel/announcements.txt
-    setfacl -m u:$name:r-- /home/$hostel/feeDefaulters.txt
-
-    # Hostel home directory permission
-    setfacl -R -m u:$hostel:rw- /home/$hostel
-
-    # mess.txt permission
-    setfacl -m u:$name:rw- /home/HAD/mess.txt
-
+    chown -R $name:HAD$hostel /home/$hostel/$roomno/$name
 done < $filePath
 
-echo "Completed set up of all permissions! Use getfacl to see the permissions."
+# Update permissions
+chmod -R 760 /home/HAD
+
+while read line
+do
+    read -a studentData <<< "$line"
+    hostel=${studentData[2]}
+    chmod -R 770 /home/$hostel
+done < $filePath
+
+while read line
+do
+    chmod -R 774 /home/$hostel/announcements.txt
+    chmod -R 774 /home/$hostel/feeDefaulters.txt
+done < $filePath
+
+while read line
+do
+    read -a studentData <<< "$line"
+    name=${studentData[0]}
+    hostel=${studentData[2]}
+    roomno=${studentData[3]}
+    chmod -R 470 /home/$hostel/$roomno/$name
+done < $filePath
+
+echo "Updated permissions!"
